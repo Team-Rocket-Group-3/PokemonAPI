ce7799da0510ecc39f0e3c7c306dc91a178284ac
SquidoCode <squidocode@hotmail.com>
2021-04-06 19:29:07 +0200

    Finished Tags

--- src/main/java/com/teamRocket/PokemonApi/domain/Ability.java
+++ src/main/java/com/teamRocket/PokemonApi/domain/Ability.java
@@ -1,17 +1,39 @@
 package com.teamRocket.PokemonApi.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @Slf4j
+@ToString
+@Entity
 public class Ability {
+    @Schema(description = "ability identifier", example = "1", required = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Schema(description = "Ability name", example = "Fly", required = true)
+    @Column
     private String name;
+    @Schema(description = "Power points", example = "8", required = true)
+    @Column
     private int pp;
+    @Schema(description = "Ability description", example = "On the turn that Fly is selected, the user will fly up high and become semi-invulnerable", required = true)
+    @Column
     private String description;
+    @Schema(description = "Pokemon which ahs this ability", example = "Pikachu", required = true)
+    @ManyToOne
+    @JoinColumn(name = "pokemon" )
+    @JsonBackReference
     private Pokemon pokemon;
+    @Schema(description = "Check if the ability is common or exclusive (true = common)", example = "true", required = true)
+    @Column
     private boolean general;
 }
