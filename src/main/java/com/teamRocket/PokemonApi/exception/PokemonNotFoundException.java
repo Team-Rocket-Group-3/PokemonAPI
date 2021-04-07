e066a0ee6f332488ce53ebc6258316905dea52a5
Verónica Pinos <veronica@MacBook-Pro-de-Veronica.local>
2021-04-07 11:45:29 +0200

    Excepciones, Controllers y Services de Pokemon y Team creados

--- src/main/java/com/teamRocket/PokemonApi/exception/PokemonNotFoundException.java
+++ src/main/java/com/teamRocket/PokemonApi/exception/PokemonNotFoundException.java
@@ -1,7 +1,20 @@
-package com.teamRocket.PokemonApi.exception;/**
-*
-* @author: veronica
-* @version Curso 2020-2021          
-*
-*/          public class PokemonNotFoundException {
+package com.teamRocket.PokemonApi.exception;
+
+/**
+ * Excepción de Pokemon no encontrado
+ * @version Curso 2020-2021
+ * @author: veronica
+ */
+public class PokemonNotFoundException extends RuntimeException {
+    public PokemonNotFoundException() {
+        super();
+    }
+
+    public PokemonNotFoundException(String message) {
+        super(message);
+    }
+
+    public PokemonNotFoundException(long id) {
+        super("Pokemon not found: " + id);
+    }
 }
--- src/main/java/com/teamRocket/PokemonApi/service/PokemonServiceImpl.java
+++ src/main/java/com/teamRocket/PokemonApi/service/PokemonServiceImpl.java
@@ -1,7 +1,24 @@
-package com.teamRocket.PokemonApi.service;/**
-*
-* @author: veronica
-* @version Curso 2020-2021          
-*
-*/          public class PokemonServiceImpl {
+package com.teamRocket.PokemonApi.service;
+
+import com.teamRocket.PokemonApi.domain.Pokemon;
+import com.teamRocket.PokemonApi.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @version Curso 2020-2021
+ * @author: veronica
+ */
+@Service
+public class PokemonServiceImpl implements PokemonService {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @Override
+    public Set<Pokemon> findAll() {
+        return pokemonRepository.findAll();
+    }
 }
