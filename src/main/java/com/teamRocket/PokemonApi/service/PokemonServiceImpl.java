8289ae0b10407bed894d1be36eca3ee553f37e0a
SquidoCode <squidocode@hotmail.com>
2021-04-08 11:39:02 +0200

    abilities web created

--- src/main/java/com/teamRocket/PokemonApi/service/PokemonServiceImpl.java
+++ src/main/java/com/teamRocket/PokemonApi/service/PokemonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.teamRocket.PokemonApi.service;
 
 import com.teamRocket.PokemonApi.domain.Pokemon;
+import com.teamRocket.PokemonApi.exception.PokemonNotFoundException;
 import com.teamRocket.PokemonApi.repository.PokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,10 @@ public class PokemonServiceImpl implements PokemonService {
     @Override
     public Set<Pokemon> findAll() {
         return pokemonRepository.findAll();
+    }
+
+    @Override
+    public Pokemon findById(Long id) {
+        return pokemonRepository.findById(id).orElseThrow(() -> new PokemonNotFoundException(id));
     }
 }
